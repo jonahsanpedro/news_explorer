5de079ae5a88d143f83c3c9adca91d908aad3b8b
jonahsanpedro <jonahsanpedro@gmail.com>
2025-09-11 13:14:43 -0700

    Final Project commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 //this file main.jsx is our javascript entry point where out App component is rendered and inserted into the DOM, by route method
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
